7cc17520f3ea61ab1490f5e6eccb3b6cd17a2d87
NimaSlmani <minsalmanig1400@gmail.com>
2023-05-31 15:01:24 +0330

    Completion of item 5

---
+++ src/components/smartmakran/FeedingChecking.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import * as yup from 'yup'
+import { Field, useForm } from 'vee-validate'
+import moment from 'moment-jalaali'
+import { useFeedingCheckingStore } from '/@src/stores/feedingChecking'
+const showFeedingCheckingModal = ref(true)
+
+import { defineProps } from 'vue'
+import { computed } from 'vue'
+import { IPond } from '/@src/interfaces/pond.interface'
+import { useFarmStore } from '/@src/stores/farm'
+import { IFeedingChecking } from '/@src/interfaces/feedingChecking.interface'
+import { useNotyf } from '/@src/composable/useNotyf'
+import { boolean } from 'yup/lib/locale'
+
+const notyf = useNotyf()
+const feedingStore = useFeedingCheckingStore()
+const farmStore = useFarmStore()
+let filteredPonds = computed<IPond[]>(() => {
+  return farmStore.currentFarm.ponds || []
+})
+const schema = yup.object({
+  amount: yup.number().required('میزان غذادهی الزامی است'),
+  type: yup.string().required('نوع غذا الزامی است'),
+  createdAt: yup.string().required('وارد کردن تاریخ الزامی است'),
+  pond: yup.number().required('وارد کردن استخر الزامی است'),
+})
+const { handleSubmit } = useForm({
+  validationSchema: schema,
+})
+
+const props = defineProps<{
+  show: boolean
+}>()
+
+const feedingCheckingForm = handleSubmit(async (values) => {
+  const { amount, type, createdAt, pond } = values
+  const time = moment.utc(createdAt).format('YYYY-MM-DD HH:mm:ss')
+  const feedingBody: IFeedingChecking = {
+    amount: amount,
+    pond: pond,
+    createdAt: time,
+  }
+  const result = await feedingStore.feedingCheckingHandler(feedingBody)
+  if (result === 201) {
+    console.log('Farm created successfully')
+    // farmStore.getFarmsList()
+  } else {
+    console.log('Farm creation failed')
+    notyf.error({
+      message: 'مزرعه ایجاد نشد، دوباره سعی کنید.',
+      duration: 2000,
+    })
+  }
+})
+</script>
+
+<template>
+  <VModal :open="show" @close="show = false" title="فرم غذادهی">
+    <template #content>
+      <form class="form-fields">
+        <div class="form-fields-field mb-20px">
+          <Field v-slot="{ field, errorMessage }" name="amount">
+            <VField>
+              <label>میزان غذادهی</label>
+              <VControl :has-error="Boolean(errorMessage)">
+                <input
+                  v-bind="field"
+                  type="text"
+                  class="input"
+                  placeholder=""
+                  autocomplete="given-name"
+                />
+                <p v-if="errorMessage" class="help is-danger">
+                  {{ errorMessage }}
+                </p>
+              </VControl>
+            </VField>
+          </Field>
+        </div>
+        <div class="form-fields-field mb-20px">
+          <Field v-slot="{ field, errorMessage }" name="pond">
+            <VField>
+              <label>استخر</label>
+              <VControl :has-error="Boolean(errorMessage)">
+                <select v-bind="field">
+                  <option value="1">استخر 1</option>
+                  <option value="2">استخر 2</option>
+                  <option value="3">استخر 3</option>
+                </select>
+                <p v-if="errorMessage" class="help is-danger">
+                  {{ errorMessage }}
+                </p>
+              </VControl>
+            </VField>
+          </Field>
+        </div>
+        <div class="form-fields-field">
+          <Field v-slot="{ field, errorMessage }" name="createdAt">
+            <VField>
+              <label>تاریخ</label>
+              <VControl :has-error="Boolean(errorMessage)">
+                <custom-date-picker
+                  v-bind="field"
+                  type="datetime"
+                  compact-time
+                ></custom-date-picker>
+              </VControl>
+              <p v-if="errorMessage" class="help is-danger">
+                {{ errorMessage }}
+              </p>
+            </VField>
+          </Field>
+        </div>
+        <div class="form-fields-field">
+          <Field v-slot="{ field, errorMessage }" name="type">
+            <VField>
+              <label>نوع غذا</label>
+              <VControl :has-error="Boolean(errorMessage)">
+                <input
+                  v-bind="field"
+                  type="text"
+                  class="input"
+                  placeholder=""
+                  autocomplete="given-name"
+                />
+                <p v-if="errorMessage" class="help is-danger">
+                  {{ errorMessage }}
+                </p>
+              </VControl>
+            </VField>
+          </Field>
+        </div>
+      </form>
+    </template>
+    <template #action>
+      <VButton color="primary" @click="feedingCheckingForm" raised>ثبت</VButton>
+    </template>
+  </VModal>
+</template>
+
+<style lang="scss">
+select {
+  height: 40px;
+  border: 1px solid rgb(222, 222, 222);
+  border-radius: 4px;
+  padding: 0 10px;
+  position: relative;
+  color: rgb(60, 60, 60);
+}
+.form-fields {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.form-fields-field {
+  width: 49%;
+}
+.mb-20px {
+  margin-bottom: 20px;
+}
+</style>
