242dc67ed82852295f6f3a9a8061043599e4b0bc
NimaSlmani <minsalmanig1400@gmail.com>
2023-06-19 08:56:46 +0330

    Adding a form to the pages related to ponds

--- src/components/smartmakran/Ponds.vue
+++ src/components/smartmakran/Ponds.vue
@@ -128,25 +128,25 @@ let closing = () => (showCreatePond.value = false)
                     <div class="card-pond-attr-icon">
                       <img src="/@src/assets/smartmakran/icons-box/bio.svg" alt="" />
                     </div>
-                    <h4>BIO MASS</h4>
+                    <h4>حجم توده زنده</h4>
                     <p>12</p>
                   </div>
                 </div>
                 <div class="card-pond-body">
                   <div class="card-pond-attr">
                     <div class="card-pond-attr-icon">
-                      <img src="/@src/assets/smartmakran/icons-box/sal.svg" alt="" />
+                      <img src="/@src/assets/smartmakran/icons-box/sal1.svg" alt="" />
                     </div>
-                    <h4>SAL</h4>
+                    <h4>میزان شوری</h4>
                     <p>32</p>
                   </div>
                 </div>
                 <div class="card-pond-body">
                   <div class="card-pond-attr">
                     <div class="card-pond-attr-icon">
-                      <img src="/@src/assets/smartmakran/icons-box/Do.svg" alt="" />
+                      <img src="/@src/assets/smartmakran/icons-box/do1.svg" alt="" />
                     </div>
-                    <h4>DO</h4>
+                    <h4>اکسیژن</h4>
                     <p>12</p>
                   </div>
                 </div>
@@ -155,7 +155,7 @@ let closing = () => (showCreatePond.value = false)
                     <div class="card-pond-attr-icon">
                       <img src="/@src/assets/smartmakran/icons-box/size.svg" alt="" />
                     </div>
-                    <h4>Size Avg</h4>
+                    <h4>میانگین سایز</h4>
                     <p>12</p>
                   </div>
                 </div>
@@ -165,7 +165,7 @@ let closing = () => (showCreatePond.value = false)
                     <div class="card-pond-attr-icon">
                       <img src="/@src/assets/smartmakran/icons-box/ph.svg" alt="" />
                     </div>
-                    <h4>PH</h4>
+                    <h4>میزان اسیدیته</h4>
                     <p>12</p>
                   </div>
                 </div>
@@ -174,7 +174,7 @@ let closing = () => (showCreatePond.value = false)
                     <div class="card-pond-attr-icon">
                       <img src="/@src/assets/smartmakran/icons-box/temp.svg" alt="" />
                     </div>
-                    <h4>TEMP</h4>
+                    <h4>دما</h4>
                     <p>12</p>
                   </div>
                 </div>
